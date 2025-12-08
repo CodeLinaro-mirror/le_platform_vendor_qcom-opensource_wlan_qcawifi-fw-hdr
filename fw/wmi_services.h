27634b177c9de7451eea22c0526c7caadfdea342
kamalakara reddy bonga <kbonga@qti.qualcomm.com>
2025-12-08 11:35:50 +0530

    qca-cmn-fw: Add new commands for halphy configuration
    Support WMI command/events to print ani error as well setting pdev
    param.
    
    Set commands:
    cfg80211tool wifiX txrx_switch_over <value>
    cfg80211tool wifiX preamble_pwr <value>
    cfg80211tool wifiX stomper_th <value>
    cfg80211tool wifiX agc_gain_value <value>
    cfg80211tool wifiX preamble_power_removal <value>
    cfg80211tool wifiX lsig_rlsig_power_scaling <value>
    cfg80211tool wifiX hesiga_power_scaling <value>
    
    Get commands:
    cfg80211tool wifiX get_ani_err
    
    Change-Id: I4005fca5581c4cdd4657edff668f77661136e2b7

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -699,6 +699,7 @@ typedef  enum  {
     WMI_SERVICE_CTRL_PATH_PEER_BA_STATS = 440, /* FW supports retrieving BlockAck stats through WMI_REQUEST_CTRL_PATH_PEER_STAT */
     WMI_SERVICE_CTRL_PATH_STA_DAR_STATS_SUPPORT = 441, /* FW supports DAR stats reporting for STA mode */
     WMI_SERVICE_APF_DATA_OFFLOAD_SUPPORT_ENABLED = 442, /* Indicates FW support for APFv6 handling offloads and disable QC data offloads */
+    WMI_SERVICE_HALPHY_ANI_ERROR_SUPPORT = 443, /* Indicates FW supports HALPHY ANI error reporting */
 
 
     WMI_MAX_EXT2_SERVICE
